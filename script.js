347adcb76b0dd6dcb396bb124f7733a75387103a
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-11 08:34:41 +0300

    adds an alert for results

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ var add = function(number1, number2){
   };
   var number1 = parseInt(prompt("Enter a number"));
   var number2 = parseInt(prompt("Enter a number"));
-  alert(add(10,5));
+  // alert(add(10,5));
+  var result = add(number1, number2);
+  alert(result);
   
   function subtract(number1, number2){
     return number1 - number2;
   };
   var number1 = parseInt(prompt("Enter a number"));
   var number2 = parseInt(prompt("Enter another number"));
-  alert(subtract(10 - 5));
+  // alert(subtract(10 - 5));
+  var result = subtract(number1, number2);
+  alert(result);
   
