c215fba1fa22e1c9301c02a97f4d57f9f881e74e
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-11 08:20:54 +0300

    files for javascript calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+var add = function(number1, number2){
+  return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter a number"));
+  alert(add(10,5));
+  
+  function subtract(number1, number2){
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter another number"));
+  alert(subtract(10 - 5));
+  
