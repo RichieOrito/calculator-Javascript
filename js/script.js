c7a962b3b2a45cb4b7c494401bb7215d5098dc27
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-18 05:52:36 +0300

    This commit added submit function event

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-var add = function(number1, number2){
+//Business (or back-end) logic:
+
+var add = function(number1, number2) {
   return number1 + number2;
-  };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter a number"));
-  // alert(add(10,5));
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+  
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
   var result = add(number1, number2);
-  alert(result);
-  
-  function subtract(number1, number2){
-    return number1 - number2;
-  };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  // alert(subtract(10 - 5));
-  var result = subtract(number1, number2);
-  alert(result);
-  
+  $("#output").text(result);
+ });
+
+}); 
